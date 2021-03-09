8ac9cf1c43201bc1b71b1ab89130d7fa4252d0f7
Kristian Freeman <kristian@kristianfreeman.com>
2021-03-09 14:47:31 -0600

    Allowlist for origins requesting the API

--- index.js
+++ index.js
@@ -3,6 +3,29 @@ addEventListener('fetch', event => {
   event.respondWith(handleRequest(event))
 })
 
+// A list of allowed origins that can access our backend API
+const allowedOrigins = [
+  'https://workers-unsplash-viewer.pages.dev',
+  "http://localhost:3000"
+]
+
+// A function that returns a set of CORS headers
+const corsHeaders = origin => ({
+  'Access-Control-Allow-Headers': '*',
+  'Access-Control-Allow-Methods': 'POST',
+  'Access-Control-Allow-Origin': origin
+})
+
+// Check the origin for this request
+// If it is included in our set of known and allowed origins, return it, otherwise
+// return a known, good origin. This effectively does not allow browsers to
+// continue requests if the origin they're requesting from doesn't match.
+const checkOrigin = request => {
+  const origin = request.headers.get("Origin")
+  const foundOrigin = allowedOrigins.find(allowedOrigin => allowedOrigin.includes(origin))
+  return foundOrigin ? foundOrigin : allowedOrigins[0]
+}
+
 // Make requests based on the request body to Unsplash API
 const getImages = async event => {
   // Parse the key "query" from a JSON body in the request
@@ -29,25 +52,22 @@ const getImages = async event => {
     link: image.links.html
   }))
 
-  // Return a JSON response with an array of images
+  // Check that the request's origin is a valid origin, allowed to access this API
+  const allowedOrigin = checkOrigin(event.request)
+
   return new Response(
     JSON.stringify(images),
-    { headers: { 'Content-type': 'application/json', ...corsHeaders } }
+    { headers: { 'Content-type': 'application/json', ...corsHeaders(allowedOrigin) } }
   )
-}
-
-// Some CORS headers to lock down our API
-const corsHeaders = {
-  'Access-Control-Allow-Headers': '*',
-  'Access-Control-Allow-Methods': 'POST',
-  'Access-Control-Allow-Origin': '*'
 }
 
 async function handleRequest(event) {
   // If an OPTIONS request comes in, return a simple
   // response with the CORS information for our app
   if (event.request.method === "OPTIONS") {
-    return new Response("OK", { headers: corsHeaders })
+    // Check that the request's origin is a valid origin, allowed to access this API
+    const allowedOrigin = checkOrigin(event.request)
+    return new Response("OK", { headers: corsHeaders(allowedOrigin) })
   }
 
   // If a POST request comes in, handle it using the getImages function
